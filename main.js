dd4964026100359c0a72766971e41793bd2e1eec
Terry Rosen <tcrosen@gmail.com>
2014-01-15 09:39:11 -0500

    modualizing compare.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var compare = require('./compare');
+
+compare({ 
+  path: 'examples\\', 
+  name: 'Config1.config' 
+}, { 
+  path: 'C:\\Terry\\Development\\Publish Tests\\MC\\', 
+  name: 'Web.config' 
+});
